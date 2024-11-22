a16654447583b7bd3b567d1e848eaa0fb9a31e26
yun <jy8767@naver.com>
2024-11-22 16:27:15 +0900

    refactor. mosquito미사일 수정중

--- Assets/scripts/controllers/M8Controller.cs
+++ Assets/scripts/controllers/M8Controller.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class M8Controller : MonoBehaviour
 {
+    public float speed = 5f; // 미사일 속도
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 초기 설정은 필요하지 않음
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 아래 방향으로 이동
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
         
     }
 }
--- Assets/scripts/controllers/MosquitoController.cs
+++ Assets/scripts/controllers/MosquitoController.cs
@@ -2,24 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MosquitoController : MonoBehaviour
+public class EnemyPlaneController : MonoBehaviour
 {
-    public float initialSpeed = 3.0f;   // 초기 이동 속도
-    public float maxSpeed = 10.0f;      // 최대 속도
-    public float acceleration = 0.1f;   // 가속도 (시간이 지날수록 속도가 증가)
-
+    public float speed = 4.0f;          // 고정 이동 속도
     public float moveRange = 5.0f;      // X축 이동 범위
     public float descendSpeed = 1.0f;   // Y축 하강 속도
 
-    private float currentSpeed;         // 현재 X축 속도
     private float startPositionX;       // 시작 X 위치 저장
     private bool isDescending = true;   // Y축 하강 여부
+
+    public GameObject missilePrefab;    // 적 미사일 프리팹
+    public Transform missileSpawnPoint; // 미사일 발사 위치
+    private float missileCooldown = 1f; // 1초 간격
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         startPositionX = transform.position.x;
-        currentSpeed = initialSpeed;    // 초기 속도로 설정
+        StartCoroutine(FireMissile()); // 미사일 자동 발사 시작
     }
 
     // Update is called once per frame
@@ -38,15 +40,22 @@ public class MosquitoController : MonoBehaviour
             }
         }
 
-        // 시간에 따라 X축 속도 증가 (최대 속도 제한 적용)
-        if (currentSpeed < maxSpeed)
-        {
-            currentSpeed += acceleration * Time.deltaTime;
-            currentSpeed = Mathf.Min(currentSpeed, maxSpeed); // 최대 속도 제한
-        }
-
-        // PingPong 함수와 증가된 속도를 사용하여 X축 이동
-        float newX = startPositionX + Mathf.PingPong(Time.time * currentSpeed, moveRange) - moveRange / 2;
+        // PingPong 함수와 고정된 속도를 사용하여 X축 이동
+        float newX = startPositionX + Mathf.PingPong(Time.time * speed, moveRange) - moveRange / 2;
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+
     }
+
+    // 미사일 발사
+    IEnumerator FireMissile()
+    {
+        while(true)
+        {
+            Instantiate(missilePrefab, missileSpawnPoint.position, Quaternion.identity);
+            yield return new WaitForSeconds(missileCooldown);
+
+        }
+    }    
+
 }

7e8c12822456e42127666c12619f3d0913b322de
cho6019 <j01040864205@gmail.com>
2024-12-01 21:23:28 +0900

    patch

--- Assets/scripts/controllers/Boss_1Controller.cs
+++ Assets/scripts/controllers/Boss_1Controller.cs
@@ -115,18 +115,17 @@ public class Boss_1Controller : MonoBehaviour
     {
         while (true)
         {
-            if(ready == 1) {
                 // 무작위 대기 시간
                 float waitTime = Random.Range(minLinearPatternInterval, maxLinearPatternInterval);
                 yield return new WaitForSeconds(waitTime);
 
                 if (!isDying) // 파괴 상태가 아닐 때만 발사
                     GetComponent<HostileWeaponProvider>().Shoot("linear");
-                    // for(int i=0; i < 4; i++) {
-                    //     audioSource.PlayOneShot(clip1);
-                    //     yield return new WaitForSeconds(0.2f);
-                    // }
-            }
+                    for(int i=0; i < 4; i++) {
+                        audioSource.PlayOneShot(clip1);
+                        yield return new WaitForSeconds(0.2f);
+                    }
+            
         }
     }
 
@@ -134,19 +133,18 @@ public class Boss_1Controller : MonoBehaviour
     {
         while (true)
         {
-            if(ready == 1) {
                 // 무작위 대기 시간
                 float waitTime = 4 + Random.Range(minCirclePatternInterval, maxCirclePatternInterval);
                 yield return new WaitForSeconds(waitTime);
 
                 if (!isDying) // 파괴 상태가 아닐 때만 발사
                     GetComponent<HostileWeaponProvider>().Shoot("circle");
-                    // yield return new WaitForSeconds(0.2f);
-                    // for(int i=0; i < 2; i++) {
-                    //     audioSource.PlayOneShot(clip2);
-                    //     yield return new WaitForSeconds(0.45f);
-                    // }
-            }
+                    yield return new WaitForSeconds(0.2f);
+                    for(int i=0; i < 2; i++) {
+                        audioSource.PlayOneShot(clip2);
+                        yield return new WaitForSeconds(0.45f);
+                    }
+            
         }
     }
 }

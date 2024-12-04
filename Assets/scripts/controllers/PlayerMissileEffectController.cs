21c45f44528ace8e37d197e9698276c9abcd6757
cho6019 <j01040864205@gmail.com>
2024-12-04 16:20:17 +0900

    patch

--- Assets/scripts/controllers/PlayerMissileEffectController.cs
+++ Assets/scripts/controllers/PlayerMissileEffectController.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class PlayerMissileEffectController : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip clip1;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 0.3f);
         audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(clip1);
     }
 
     // Update is called once per frame
